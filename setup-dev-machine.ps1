0110923a58384982d2e9892d0731ec5a14d88bfe
Matthew Fortunka <mafortun@microsoft.com>
2021-05-07 14:54:19 +0100

    added extensions and additional files

--- setup-dev-machine.ps1
+++ setup-dev-machine.ps1
@@ -6,16 +6,16 @@ $Packages = @( `
             [pscustomobject]@{ name='curl';params='' },`
             [pscustomobject]@{ name='azure-cli';params='' },`
             [pscustomobject]@{ name='microsoft-edge-insider-dev';params='' },`
-            [pscustomobject]@{ name='vscode';params='' },`
-            [pscustomobject]@{ name='vscode-vsliveshare';params=''},`
-            [pscustomobject]@{ name='azurefunctions-vscode';params='' },`
-            [pscustomobject]@{ name='azureaccount-vscode';params='' },`
-            [pscustomobject]@{ name='vscode-csharp';params='' },`
-            [pscustomobject]@{ name='vscode-powershell';params='' },`
-            [pscustomobject]@{ name='vscode-csharpextensions';params='' },`
-            [pscustomobject]@{ name='vscode-azurerm-tools';params='' },`
             [pscustomobject]@{ name='bicep';params='' },`
-            [pscustomobject]@{ name='azure-functions-core-tools';params='/x64' }`
+            [pscustomobject]@{ name='azure-functions-core-tools';params='/x64' },`
+            [pscustomobject]@{ name='microsoft-windows-terminal';params='' },`
+            [pscustomobject]@{ name='dotnetcore-sdk';params='' },`
+            [pscustomobject]@{ name='dotnet-sdk';params='' },`
+            [pscustomobject]@{ name='git';params='' },`
+            [pscustomobject]@{ name='omnisharp';params='' },`
+            [pscustomobject]@{ name='kubernetes-cli';params='' },`
+            [pscustomobject]@{ name='docker-cli';params='' },`
+            [pscustomobject]@{ name='docker-desktop';params='' }`
             )
 
 Foreach ($Package in $Packages)
@@ -28,6 +28,98 @@ Foreach ($Package in $Packages)
         choco install $Package.name -y
     }
 }
+
+$extensions = @(`
+                "4ops.terraform",`
+                "auchenberg.vscode-browser-preview",`  
+                "AwesomeAutomationTeam.azureautomation",`
+                "azuredevspaces.azds",`
+                "bencoleman.armview",`
+                "dan-c-underwood.arm",`
+                "dbaeumer.vscode-eslint",`
+                "DotJoshJohnson.xml",`
+                "eamodio.gitlens",`
+                "esbenp.prettier-vscode",`
+                "florian.jwt",`
+                "formulahendry.azure-storage-explorer",`
+                "GitHub.codespaces",`
+                "GitHub.vscode-pull-request-github",`
+                "golang.go",`
+                "hashicorp.terraform",`
+                "hediet.vscode-drawio",`
+                "huacat.pink-theme",`
+                "humao.rest-client",`
+                "laszer25.terraform-azure-autocomplete",`
+                "marmac.xslt-action",`
+                "mindaro-dev.file-downloader",`
+                "mindaro.mindaro",`
+                "momoto.binary-viewer",`
+                "ms-azure-devops.azure-pipelines",`
+                "ms-azuretools.vscode-apimanagement",`
+                "ms-azuretools.vscode-azureAPIConnections",`
+                "ms-azuretools.vscode-azureappservice",`
+                "ms-azuretools.vscode-azurefunctions",`
+                "ms-azuretools.vscode-azurelogicapps",`
+                "ms-azuretools.vscode-azureresourcegroups",`
+                "ms-azuretools.vscode-azurestorage",`
+                "ms-azuretools.vscode-azureterraform",`
+                "ms-azuretools.vscode-azurevirtualmachines",`
+                "ms-azuretools.vscode-bicep",`
+                "ms-azuretools.vscode-cosmosdb",`
+                "ms-azuretools.vscode-docker",`
+                "ms-azuretools.vscode-logicapps",`
+                "ms-dotnettools.csharp",`
+                "ms-dotnettools.vscode-dotnet-runtime",`
+                "ms-kubernetes-tools.vscode-aks-tools",`
+                "ms-kubernetes-tools.vscode-kubernetes-tools",`
+                "ms-mssql.mssql",`
+                "ms-python.python",`
+                "ms-toolsai.jupyter",`
+                "ms-vscode-remote.remote-containers",`
+                "ms-vscode-remote.remote-ssh",`
+                "ms-vscode-remote.remote-ssh-edit",`
+                "ms-vscode-remote.remote-wsl",`
+                "ms-vscode-remote.vscode-remote-extensionpack",`
+                "ms-vscode.azure-account",`
+                "ms-vscode.azurecli",`
+                "ms-vscode.js-debug-nightly",`
+                "ms-vscode.powershell",`
+                "ms-vscode.vscode-node-azure-pack",`
+                "ms-vsliveshare.vsliveshare",`
+                "ms-vsts.team",`
+                "msazurermtools.azurerm-vscode-tools",`
+                "msjsdiag.debugger-for-chrome",`
+                "octref.vetur",`
+                "redhat.fabric8-analytics",`
+                "redhat.java",`
+                "redhat.vscode-commons",`
+                "redhat.vscode-xml",`
+                "redhat.vscode-yaml",`
+                "ritwickdey.LiveServer",`
+                "tht13.python",`
+                "tonybaloney.vscode-pets",`
+                "VisualStudioExptTeam.vscodeintellicode",`
+                "VisualStudioOnlineApplicationInsights.application-insights",`
+                "vsciot-vscode.azure-iot-toolkit",`
+                "vscjava.vscode-java-debug",`
+                "vscjava.vscode-java-dependency",`
+                "vscjava.vscode-java-pack",`
+                "vscjava.vscode-java-test",`
+                "vscjava.vscode-maven",`
+                "vscode-icons-team.vscode-icons",`
+                "WASTeamAccount.WebTemplateStudio-dev-nightly",`
+                "ytechie.armviz",`
+                "yzane.markdown-pdf"
+            )
+
+Foreach ($extension in $extensions)
+{
+    code --install-extension $extension
+}
+
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 
 
 Write-Host "Script Complete"
